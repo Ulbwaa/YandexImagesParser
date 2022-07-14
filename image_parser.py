cafa11db77f1ee2f4d175b1f7c2f0a40192aff4f
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2022-07-14 10:22:46 +0300

    Fix type issues, improve logic

--- image_parser.py
+++ image_parser.py
@@ -1,66 +1,50 @@
-import requests
 import json
+from typing import Literal, NamedTuple
+
+import requests
 from fake_headers import Headers
 from bs4 import BeautifulSoup as bs4
 
 
-class Size:
-    def __init__(self):
-        self.large = "large"
-        self.medium = "medium"
-        self.small = "small"
+SIZES = Literal["small", "medium", "large"]
 
 
-class Preview:
-    def __init__(self, url: str, width: int, height: int):
-        self.url = url
-        self.width = width
-        self.height = height
-        self.size = str(width) + "*" + str(height)
+class Preview(NamedTuple):
+    url: str
+    width: int
+    height: int
 
 
-class Result:
-    def __init__(
-        self,
-        title: (str, None),
-        description: (str, None),
-        domain: str,
-        url: str,
-        width: int,
-        height: int,
-        preview: Preview,
-    ):
-        self.title = title
-        self.description = description
-        self.domain = domain
-        self.url = url
-        self.width = width
-        self.height = height
-        self.size = str(width) + "*" + str(height)
-        self.preview = preview
+class Result(NamedTuple):
+    title: str
+    description: str
+    domain: str
+    url: str
+    width: int
+    height: int
+    preview: Preview
 
 
 class YandexImage:
+    _URL = "https://yandex.ru/images/search"
+
     def __init__(self):
-        self.size = Size()
         self.headers = Headers(headers=True).generate()
         self.version = "1.0-release"
         self.about = "Yandex Images Parser"
 
-    def search(self, query: str, sizes: Size = "large") -> list:
+    def search(self, query: str, sizes: SIZES = "large") -> list[str]:
         request = requests.get(
-            "https://yandex.ru/images/search",
+            self._URL,
             params={"text": query, "nomisspell": 1, "noreask": 1, "isize": sizes},
             headers=self.headers,
         )
 
+        output = []
+
         soup = bs4(request.text, "html.parser")
-        items_place = soup.find("div", {"class": "serp-list"})
-        output = list()
-        try:
-            items = items_place.find_all("div", {"class": "serp-item"})
-        except AttributeError:
-            return output
+        items_place, *__ = soup.find_all("div", {"class": "serp-list"})
+        items = items_place.find_all("div", {"class": "serp-item"})
 
         for item in items:
             data = json.loads(item.get("data-bem"))
@@ -69,30 +53,21 @@ class YandexImage:
             image_height = data["serp-item"]["preview"][0]["h"]
 
             snippet = data["serp-item"]["snippet"]
-            try:
-                title = snippet["title"]
-            except KeyError:
-                title = None
-            try:
-                description = snippet["text"]
-            except KeyError:
-                description = None
-            domain = snippet["domain"]
 
             preview = "https:" + data["serp-item"]["thumb"]["url"]
             preview_width = data["serp-item"]["thumb"]["size"]["width"]
             preview_height = data["serp-item"]["thumb"]["size"]["height"]
 
-            output.append(
-                Result(
-                    title,
-                    description,
-                    domain,
-                    image,
-                    image_width,
-                    image_height,
-                    Preview(preview, preview_width, preview_height),
-                )
+            result = Result(
+                snippet.get("title", ""),
+                snippet.get("text", ""),
+                snippet.get("domain", ""),
+                image,
+                image_width,
+                image_height,
+                Preview(preview, preview_width, preview_height),
             )
+
+            output.append(result)
 
         return output
