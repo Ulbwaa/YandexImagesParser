8cb8998744599626f00b2925a57c1931bb4850ff
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2022-07-14 09:27:37 +0300

    Format commit

--- image_parser.py
+++ image_parser.py
@@ -6,36 +6,37 @@ from bs4 import BeautifulSoup as bs4
 
 class Size:
     def __init__(self):
-        self.large = 'large'
-        self.medium = 'medium'
-        self.small = 'small'
+        self.large = "large"
+        self.medium = "medium"
+        self.small = "small"
 
 
 class Preview:
-    def __init__(self, url: str,
-                 width: int,
-                 height: int):
+    def __init__(self, url: str, width: int, height: int):
         self.url = url
         self.width = width
         self.height = height
-        self.size = str(width) + '*' + str(height)
+        self.size = str(width) + "*" + str(height)
 
 
 class Result:
-    def __init__(self, title: (str, None),
-                 description: (str, None),
-                 domain: str,
-                 url: str,
-                 width: int,
-                 height: int,
-                 preview: Preview):
+    def __init__(
+        self,
+        title: (str, None),
+        description: (str, None),
+        domain: str,
+        url: str,
+        width: int,
+        height: int,
+        preview: Preview,
+    ):
         self.title = title
         self.description = description
         self.domain = domain
         self.url = url
         self.width = width
         self.height = height
-        self.size = str(width) + '*' + str(height)
+        self.size = str(width) + "*" + str(height)
         self.preview = preview
 
 
@@ -43,20 +44,18 @@ class YandexImage:
     def __init__(self):
         self.size = Size()
         self.headers = Headers(headers=True).generate()
-        self.version = '1.0-release'
-        self.about = 'Yandex Images Parser'
+        self.version = "1.0-release"
+        self.about = "Yandex Images Parser"
 
-    def search(self, query: str, sizes: Size = 'large') -> list:
-        request = requests.get('https://yandex.ru/images/search',
-                               params={"text": query,
-                                       "nomisspell": 1,
-                                       "noreask": 1,
-                                       "isize": sizes
-                                       },
-                               headers=self.headers)
+    def search(self, query: str, sizes: Size = "large") -> list:
+        request = requests.get(
+            "https://yandex.ru/images/search",
+            params={"text": query, "nomisspell": 1, "noreask": 1, "isize": sizes},
+            headers=self.headers,
+        )
 
-        soup = bs4(request.text, 'html.parser')
-        items_place = soup.find('div', {"class": "serp-list"})
+        soup = bs4(request.text, "html.parser")
+        items_place = soup.find("div", {"class": "serp-list"})
         output = list()
         try:
             items = items_place.find_all("div", {"class": "serp-item"})
@@ -65,29 +64,35 @@ class YandexImage:
 
         for item in items:
             data = json.loads(item.get("data-bem"))
-            image = data['serp-item']['img_href']
-            image_width = data['serp-item']['preview'][0]['w']
-            image_height = data['serp-item']['preview'][0]['h']
+            image = data["serp-item"]["img_href"]
+            image_width = data["serp-item"]["preview"][0]["w"]
+            image_height = data["serp-item"]["preview"][0]["h"]
 
-            snippet = data['serp-item']['snippet']
+            snippet = data["serp-item"]["snippet"]
             try:
-                title = snippet['title']
+                title = snippet["title"]
             except KeyError:
                 title = None
             try:
-                description = snippet['text']
+                description = snippet["text"]
             except KeyError:
                 description = None
-            domain = snippet['domain']
+            domain = snippet["domain"]
 
-            preview = 'https:' + data['serp-item']['thumb']['url']
-            preview_width = data['serp-item']['thumb']['size']['width']
-            preview_height = data['serp-item']['thumb']['size']['height']
+            preview = "https:" + data["serp-item"]["thumb"]["url"]
+            preview_width = data["serp-item"]["thumb"]["size"]["width"]
+            preview_height = data["serp-item"]["thumb"]["size"]["height"]
 
-            output.append(Result(title, description, domain, image,
-                                 image_width, image_height,
-                                 Preview(preview, preview_width, preview_height)))
+            output.append(
+                Result(
+                    title,
+                    description,
+                    domain,
+                    image,
+                    image_width,
+                    image_height,
+                    Preview(preview, preview_width, preview_height),
+                )
+            )
 
         return output
-
-
